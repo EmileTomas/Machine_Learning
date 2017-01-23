1fc0cbc814407630e21c8fcdc83f15ad9887612a
unknown <>
2017-01-23 12:36:24 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Introduction
+
+### What does this repository do
+
+This is repository to record the procedure of learning Machine Learning. I learned machine learning courses provided by Andrew Ng. in Coursera as initiation. At present stage, I choose to do some ML practice using  *Machine Learning in Action (Peter Harrington)* as textbook. What's different is that I use MATLAB to fulfill the algorithm in the book instead of Python.
+
---
+++ kNN/kNN_classify.m
@@ -0,0 +1,29 @@
+function result=kNN_classify(index,dataSet,k)
+%   Data: 2017-1-19
+%   -----------------------------------------
+%   Args:
+%   index: 
+%           The dimensions of target
+%   dataSet:   
+%           The dimensions of training data set and result
+%           Example:
+%            [d11 d12 d13 d14 result1;
+%             d21 d22 d23 d24 result2;
+%             d31 d32 d33 d34 result3;]
+%  k:
+%           Choose the closest k points to predict the result
+dataSetSize=size(dataSet,1);
+dataSetIndex=dataSet(:,1:(size(dataSet,2)-1));
+dataSetResult=dataSet(:,size(dataSet,2));
+
+diffMat=repmat(index,dataSetSize,1)-dataSetIndex;
+sqDiffMat=diffMat.^2;
+sqDistances=sum(sqDiffMat,2);
+distances=sqDistances.^0.5;
+[sortResult,sortedDistIndicies]=sort(distances);
+ 
+closestResult=dataSetResult(sortedDistIndicies);
+resultRequency=tabulate(closestResult(1:k));
+result=resultRequency(1,1);
+end
+
